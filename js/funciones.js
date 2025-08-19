fc83593cd8c74c96e413f5d8200ec19ff54631c8
stp20 <stroncoso05@gmail.com>
2025-08-19 13:48:39 -0400

    prueba

--- js/funciones.js
+++ js/funciones.js
@@ -1,33 +1,33 @@
 
 
 const dogs = [
-    { nombre: "Firulais", desc: "Juguetón y cariñoso.", img: "../img/perros/perrito1.jpg" },
-    { nombre: "Rex", desc: "Fiel guardián.", img: "../img/perros/perrito2.jpg" },
-    { nombre: "Luna", desc: "Dulce y tranquila.", img: "../img/perros/perrito3.jpg" },
-    { nombre: "Max", desc: "Ama correr en el parque.", img: "../img/perros/perrito4.jpg" },
-    { nombre: "Bobby", desc: "Le encantan los niños.", img: "../img/perros/perrito5.jpg" },
-    { nombre: "Rocky", desc: "Valiente y leal.", img: "../img/perros/perrito6.jpg" },
-    { nombre: "Toby", desc: "Muy obediente.", img: "../img/perros/perrito7.webp" },
-    { nombre: "Zeus", desc: "Grande y protector.", img: "../img/perros/perrito8.webp" },
-    { nombre: "Chispa", desc: "Energía sin fin.", img: "../img/perros/perrito9.jpg" },
-    { nombre: "Coco", desc: "Muy curioso.", img: "../img/perros/perrito10.jpg" },
-    { nombre: "Simón", desc: "Le encantan las siestas.", img: "../img/perros/perrito11.jpg" },
-    { nombre: "Duke", desc: "Elegante y noble.", img: "../img/perros/perrito12.jpg" }
+    { nombre: "Firulais", desc: "Juguetón y cariñoso.", img: "../assets/img/perros/perrito1.jpg" },
+    { nombre: "Rex", desc: "Fiel guardián.", img: "../assets/img/perros/perrito2.jpg" },
+    { nombre: "Luna", desc: "Dulce y tranquila.", img: "../assets/img/perros/perrito3.jpg" },
+    { nombre: "Max", desc: "Ama correr en el parque.", img: "../assets/img/perros/perrito4.jpg" },
+    { nombre: "Bobby", desc: "Le encantan los niños.", img: "../assets/img/perros/perrito5.jpg" },
+    { nombre: "Rocky", desc: "Valiente y leal.", img: "../assets/img/perros/perrito6.jpg" },
+    { nombre: "Toby", desc: "Muy obediente.", img: "../assets/img/perros/perrito7.webp" },
+    { nombre: "Zeus", desc: "Grande y protector.", img: "../assets/img/perros/perrito8.webp" },
+    { nombre: "Chispa", desc: "Energía sin fin.", img: "../assets/img/perros/perrito9.jpg" },
+    { nombre: "Coco", desc: "Muy curioso.", img: "../assets/img/perros/perrito10.jpg" },
+    { nombre: "Simón", desc: "Le encantan las siestas.", img: "../assets/img/perros/perrito11.jpg" },
+    { nombre: "Duke", desc: "Elegante y noble.", img: "../assets/img/perros/perrito12.jpg" }
 ];
 
 const cats = [
-    { nombre: "Michi", img: "../img/gatos/gato1.jpg", desc: "Duerme casi todo el día." },
-    { nombre: "Nina", img: "../img/gatos/gato2.jpg", desc: "Muy cariñosa." },
-    { nombre: "Milo", img: "../img/gatos/gato3.webp", desc: "Le encanta jugar." },
-    { nombre: "Tom", img: "../img/gatos/gato4.jpg", desc: "Explorador nato." },
-    { nombre: "Lola", img: "../img/gatos/gato5.webp", desc: "Muy tierna." },
-    { nombre: "Chispa", img: "../img/gatos/gato6.jpg", desc: "Travieso y curioso." },
-    { nombre: "Kira", img: "../img/gatos/gato7.jpg", desc: "Compañera fiel." },
-    { nombre: "Felix", img: "../img/gatos/gato8.jpg", desc: "Muy independiente." },
-    { nombre: "Tina", img: "../img/gatos/gato9.jpg", desc: "Le gusta acurrucarse." },
-    { nombre: "Oliver", img: "../img/gatos/gato10.jpg", desc: "Siempre curioso." },
-    { nombre: "Sasha", img: "../img/gatos/gato11.webp", desc: "Muy activa." },
-    { nombre: "Nube", img: "../img/gatos/gato12.jpg", desc: "Muy tranquila." }
+    { nombre: "Michi", img: "../assets/img/gatos/gato1.jpg", desc: "Duerme casi todo el día." },
+    { nombre: "Nina", img: "../assets/img/gatos/gato2.jpg", desc: "Muy cariñosa." },
+    { nombre: "Milo", img: "../assets/img/gatos/gato3.webp", desc: "Le encanta jugar." },
+    { nombre: "Tom", img: "../assets/img/gatos/gato4.jpg", desc: "Explorador nato." },
+    { nombre: "Lola", img: "../assets/img/gatos/gato5.webp", desc: "Muy tierna." },
+    { nombre: "Chispa", img: "../assets/img/gatos/gato6.jpg", desc: "Travieso y curioso." },
+    { nombre: "Kira", img: "../assets/img/gatos/gato7.jpg", desc: "Compañera fiel." },
+    { nombre: "Felix", img: "../assets/img/gatos/gato8.jpg", desc: "Muy independiente." },
+    { nombre: "Tina", img: "../assets/img/gatos/gato9.jpg", desc: "Le gusta acurrucarse." },
+    { nombre: "Oliver", img: "../assets/img/gatos/gato10.jpg", desc: "Siempre curioso." },
+    { nombre: "Sasha", img: "../assets/img/gatos/gato11.webp", desc: "Muy activa." },
+    { nombre: "Nube", img: "../assets/img/gatos/gato12.jpg", desc: "Muy tranquila." }
 ];
 
 const allPets = dogs.concat(cats);
